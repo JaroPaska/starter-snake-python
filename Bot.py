8a97f69207e9a4852eeafc7853832468d871e843
Hodobox <samkogursky@gmail.com>
2021-02-06 15:58:21 +0100

    Add untested GravityBot

--- Bot.py
+++ Bot.py
@@ -49,7 +49,7 @@ def make_graph(h,w,board): # given board makes empty graph
     graph[ f['y'] ][ f['x'] ].char = 'o'
 
   for s in board['snakes']:
-    for pt in s['body'][:-1]:
+    for pt in s['body']:
       graph[ pt['y'] ][ pt['x'] ].char = '#'
   return graph
 
@@ -134,6 +134,7 @@ class EatBot(Bot):
     # BFS on the graph being careful
     graph = make_graph(h,w,board)
     best_food = find_food(data, graph, risk_heads = False)
+    # if careful didn't work, and we're desperate, try not being careful
     if best_food == (-1,-1) and data['you']['health'] <= (h+w)//1.47:
       graph = make_graph(h,w,board)
       best_food = find_food(data,graph, risk_heads = True)
@@ -168,3 +169,121 @@ class EatBot(Bot):
 
       # didn't find empty square, guess I'll die
       return "up"
+
+class GravityBot(Bot):
+
+  def __init__(self):
+    self.EMPTY = (1,-0.1)
+    self.FOOD = (5,-0.05)
+    self.TAIL = (0.1,-0.1)
+    self.BIGGER_HEAD = (-30,28)
+    self.BLOCKED = (-0.2,0.15)
+
+  def evaluate(graph, data, y, x):
+    val = None
+    if graph[y][x].char == '.':
+      val = self.EMPTY
+    elif graph[y][x].char == 'o':
+      val = list(self.FOOD)
+      val[0] += (1.01**(3*(100-data['you']['health'])))
+    else:
+      # head or tail
+      my_size = len(data['you']['body'])
+      for snake in data['board']['snakes']:
+        if (x,y) == (snake['body'][-1]['x'],snake['body'][-1]['y']):
+          val = self.TAIL
+          break
+
+        if snake['id'] == data['you']['id'] or len(snake['body']) < my_size:
+          continue
+
+        if (x,y) == (snake['body'][0]['x'],snake['body'][1]['y']):
+          val = self.BIGGER_HEAD
+          break
+
+    if val == None:
+      val = self.BLOCKED
+
+    return list(val)
+
+  def propagate_gravity(graph, data, ratings, sy, sx):
+
+    val = evaluate(graph, data, sy, sx)
+    h = data['board']['height']
+    w = data['board']['width']
+    tmp = [ [ [0,0] for _ in range(data['board']['width']) ] for _ in range(data['board']['height']) ]
+
+    if sy >= 0 and sy < h and sx >= 0 and sy < w:
+      tmp[sy][sx] = val
+
+    q = deque()
+
+    for d in range(4):
+      nx = sx + dx[d]
+      ny = sy + dy[d]
+      if nx < 0 or ny < 0 or nx == w or ny == h or graph[ny][nx].char == '#':
+        continue
+
+      tmp[ny][nx] = val
+      q.append(ny)
+      q.append(nx)
+
+    while len(q):
+      y = q.popleft()
+      x = q.popleft()
+
+      for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        # ignore bad squares
+        if nx < 0 or ny < 0 or nx == w or ny == h or graph[ny][nx].char == '#' or tmp[ny][nx] != [0,0]:
+          continue
+
+        new_val = tmp[y][x]
+        new_val[0] += new_val[1]
+
+        if new_val[0] * tmp[y][x][0] <= 0:
+          continue
+
+        tmp[ny][nx] = new_val
+        q.push(ny)
+        q.push(nx)
+
+    for y in range(0,h):
+      for x in range(0,w):
+        ratings[y][x] += tmp[y][x][0]
+
+  def move(self, data):
+
+    board = data['board']
+    myID = data['you']['id']
+
+    h = board['height']
+    w = board['width']
+
+    # BFS on the graph being careful
+    graph = make_graph(h,w,board)
+    ratings = [ [0 for _ in range(w)] for _ in range(h) ]
+
+    for y in range(-1,h+1):
+      for x in range(-1,w+1):
+        propagate_gravity(graph, data, ratings, y, x)
+
+    sy = data['you']['head']['y']
+    sx = data['you']['head']['x']
+
+    best = -10000
+    best_dir = 0
+
+    for d in range(4):
+      nx = sx + dx[d]
+      ny = sy + dy[d]
+      if nx < 0 or ny < 0 or nx == w or ny == h:
+        continue
+
+      if ratings[ny][nx] > best:
+        best = ratings[ny][nx]
+        best_dir = d
+
+    return dir_to_word[best_dir]
